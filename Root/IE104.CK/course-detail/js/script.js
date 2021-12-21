db13f657a3a1b7caad30bdb6c7978519f119ac40
19521697 <19521697@gm.uit.edu.vn>
2021-12-21 07:47:51 +0700

    Add remaining site

---
+++ Root/IE104.CK/course-detail/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+document.getElementById("join").onclick = function() {
+	window.open("../learn/Bai1.html","_self");
+}
---
+++ Root/IE104.CK/courses/js/script.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+document.getElementById("html1").onclick = function() {
+	window.open("../course-detail/course-detail.html","_self");
+}
